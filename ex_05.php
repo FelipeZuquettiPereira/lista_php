a0c3c8af0f7e006d4a09fadf7599b549b92575a8
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-08-07 12:46:24 -0300

    5 pronta

--- ex_05.php
+++ ex_05.php
@@ -1,5 +1,20 @@
 <?php
 
-    
+function analisarTexto($texto){
+
+    $palavras = str_word_count($texto);
+    $caracteres = mb_strlen($texto);
+    $vogais = preg_match_all('/[aeiouáéíóúãõâêîôû]/ui', $texto);
+    $consoantes = preg_match_all('/[b-df-hj-np-tv-z]/i', $texto);
+
+    echo "Palavras: ". $palavras ."<br>"; 
+    echo "Caracteres: ". $caracteres ."<br>";
+    echo "Vogais: ". $vogais ."<br>"; 
+    echo "Consoantes: ". $consoantes; 
+}
+ 
+$texto = "Texto de exemplo";
+
+analisarTexto($texto);
 
 ?>
